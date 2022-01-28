5411e1bdd09f4e826afd5d69054c173f779cd2f8
Indoraptor1133242 <97930673+Indoraptor1133242@users.noreply.github.com>
2022-01-27 19:28:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeimage() {
+    document.getElementById("audio").play();
+    window.alert("Es una imagen");
+    document.getElementById("musk").src = "https://fondosmil.com/fondo/79895.jpg";
+}
+
+function quitar() {
+    document.getElementById("musk").src = "https://image.cnbcfm.com/api/v1/image/106820840-16100466222021-01-07t183104z_1584998285_rc263l9bivig_rtrmadp_0_usa-stocks-tesla-facebook.jpeg?v=1610046717&w=929&h=523";
+}
